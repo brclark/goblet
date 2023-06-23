26576b4d8c373b85692a07e20c74c5483ba905b3
raj3m8@gmail.com <raj3m8@gmail.com>
2023-06-23 15:32:02 -0500

    Update reqs

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 def create_app():
     app = Flask(__name__)
     app.config['DEBUG'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://khcwijqquknjzc:a724588a50a8a689dea14299f55faa50560f9640321056783bae130416738eab@ec2-34-236-56-112.compute-1.amazonaws.com:5432/d9dfei58nqd9t4'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://khcwijqquknjzc:a724588a50a8a689dea14299f55faa50560f9640321056783bae130416738eab@ec2-34-236-56-112.compute-1.amazonaws.com:5432/d9dfei58nqd9t4'
     app.config['SQLALCHEMY_ECHO'] = True
 
     with app.app_context():
