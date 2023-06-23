32873d581ca07ccd4f5df67ebbf050e025d174b0
raj3m8@gmail.com <raj3m8@gmail.com>
2023-06-23 15:02:59 -0500

    Postgres url

--- api/api/main.py
+++ api/api/main.py
@@ -7,7 +7,7 @@ from time import time
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://goblet:goblet-api@localhost:8889/goblet'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://khcwijqquknjzc:a724588a50a8a689dea14299f55faa50560f9640321056783bae130416738eab@ec2-34-236-56-112.compute-1.amazonaws.com:5432/d9dfei58nqd9t4'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
